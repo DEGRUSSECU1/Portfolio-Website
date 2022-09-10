dd4be691b0f4252b5bea8ad810f6f2e0fdecf566
Diego Russo <diego25.russo@gmail.com>
2022-09-10 21:13:36 +0100

    icons added, icons container and icons style added, creating function in main.js to switch sections

---
+++ main.js
@@ -0,0 +1,14 @@
+//selecting all the sections that have a class of section
+const sections = Document.querySelectorAll(".section");
+/* selecting the container that contains the buttons */
+const sectBtns = Document.querySelectorAll(".controls");
+//selecting the buttons 
+const secBtn = Document.querySelectorAll(".cotrol");
+//select the body with the class of main-content
+const allSelections = document.querySelector(".main-content")
+
+//Funtion to switch each section 
+
+const pageTransitions = () => {
+    //create a for loop to go to each button 
+}
